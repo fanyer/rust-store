d2ea80495593bc210dbec212550c55a758bb8a96
樊烨尔 <iofanyer@gmail.com>
2016-09-24 03:22:26 +0800

    dirname write

---
+++ src/dirname/tests/test_dirname.rs
@@ -0,0 +1,37 @@
+extern crate common;
+
+
+use common::macros::*;
+use common::util::*;
+
+
+#[test]
+fn test_path_with_trailing_slashes() {
+    new_ucmd!()
+        .arg("/root/alpha/beta/gamma/delta/epsilon/omega//")
+        .run()
+        .stdout_is("/root/alpha/beta/gamma/delta/epsilon");
+}
+
+#[test]
+fn test_path_without_trailing_slashes() {
+    new_ucmd!()
+        .arg("/root/alpha/beta/gamma/delta/epsilon/omega")
+        .run()
+        .stdout_is("/root/alpha/beta/gamma/delta/epsilon");
+}
+
+#[test]
+fn test_root() {
+    new_ucmd!().arg("/").run().stdout_is("/");
+}
+
+#[test]
+fn test_pwd() {
+    new_ucmd!().arg(".").run().stdout_is(".");
+}
+
+#[test]
+fn test_empty() {
+    new_ucmd!().arg("").run().stdout_is(".");
+}
