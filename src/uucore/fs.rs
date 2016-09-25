880bfab6ea4305a8b69cc78ee425fea45395fea4
樊烨尔 <iofanyer@gmail.com>
2016-09-26 02:14:28 +0800

    include euchre

---
+++ src/uucore/fs.rs
@@ -0,0 +1,174 @@
+// This file is part of the uutils coreutils package.
+//
+// (c) Joseph Crail <jbcrail@gmail.com>
+// (c) Jian Zeng <anonymousknight96 AT gmail.com>
+//
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code.
+//
+
+#[cfg(unix)]
+use super::libc;
+use std::env;
+use std::fs;
+use std::io::{Error, ErrorKind};
+use std::io::Result as IOResult;
+use std::path::{Component, Path, PathBuf};
+use std::borrow::Cow;
+
+#[cfg(unix)]
+pub fn resolve_relative_path<'a>(path: &'a Path) -> Cow<'a, Path> {
+    if path.is_absolute() {
+        return path.into();
+    }
+    let mut result = env::current_dir().unwrap_or(PathBuf::from("/"));
+    for comp in path.components() {
+        match comp {
+            Component::ParentDir => {
+                result.pop();
+            }
+            Component::CurDir => (),
+            Component::Normal(s) => result.push(s),
+            _ => unreachable!(),
+        }
+    }
+    result.into()
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub enum CanonicalizeMode {
+    None,
+    Normal,
+    Existing,
+    Missing,
+}
+
+fn resolve<P: AsRef<Path>>(original: P) -> IOResult<PathBuf> {
+    const MAX_LINKS_FOLLOWED: u32 = 255;
+    let mut followed = 0;
+    let mut result = original.as_ref().to_path_buf();
+    loop {
+        if followed == MAX_LINKS_FOLLOWED {
+            return Err(Error::new(ErrorKind::InvalidInput, "maximum links followed"));
+        }
+
+        match fs::symlink_metadata(&result) {
+            Err(e) => return Err(e),
+            Ok(ref m) if !m.file_type().is_symlink() => break,
+            Ok(..) => {
+                followed += 1;
+                match fs::read_link(&result) {
+                    Ok(path) => {
+                        result.pop();
+                        result.push(path);
+                    }
+                    Err(e) => {
+                        return Err(e);
+                    }
+                }
+            }
+        }
+    }
+    Ok(result)
+}
+
+pub fn canonicalize<P: AsRef<Path>>(original: P, can_mode: CanonicalizeMode) -> IOResult<PathBuf> {
+    // Create an absolute path
+    let original = original.as_ref();
+    let original = if original.is_absolute() {
+        original.to_path_buf()
+    } else {
+        env::current_dir().unwrap().join(original)
+    };
+
+    let mut result = PathBuf::new();
+    let mut parts = vec![];
+
+    // Split path by directory separator; add prefix (Windows-only) and root
+    // directory to final path buffer; add remaining parts to temporary
+    // vector for canonicalization.
+    for part in original.components() {
+        match part {
+            Component::Prefix(_) |
+            Component::RootDir => {
+                result.push(part.as_os_str());
+            }
+            Component::CurDir => (),
+            Component::ParentDir => {
+                parts.pop();
+            }
+            Component::Normal(_) => {
+                parts.push(part.as_os_str());
+            }
+        }
+    }
+
+    // Resolve the symlinks where possible
+    if !parts.is_empty() {
+        for part in parts[..parts.len() - 1].iter() {
+            result.push(part);
+
+            if can_mode == CanonicalizeMode::None {
+                continue;
+            }
+
+            match resolve(&result) {
+                Err(e) => {
+                    match can_mode {
+                        CanonicalizeMode::Missing => continue,
+                        _ => return Err(e),
+                    }
+                }
+                Ok(path) => {
+                    result.pop();
+                    result.push(path);
+                }
+            }
+        }
+
+        result.push(parts.last().unwrap());
+
+        match resolve(&result) {
+            Err(e) => {
+                if can_mode == CanonicalizeMode::Existing {
+                    return Err(e);
+                }
+            }
+            Ok(path) => {
+                result.pop();
+                result.push(path);
+            }
+        }
+    }
+    Ok(result)
+}
+
+#[cfg(unix)]
+pub fn is_stdin_interactive() -> bool {
+    unsafe { libc::isatty(libc::STDIN_FILENO) == 1 }
+}
+
+#[cfg(windows)]
+pub fn is_stdin_interactive() -> bool {
+    false
+}
+
+#[cfg(unix)]
+pub fn is_stdout_interactive() -> bool {
+    unsafe { libc::isatty(libc::STDOUT_FILENO) == 1 }
+}
+
+#[cfg(windows)]
+pub fn is_stdout_interactive() -> bool {
+    false
+}
+
+#[cfg(unix)]
+pub fn is_stderr_interactive() -> bool {
+    unsafe { libc::isatty(libc::STDERR_FILENO) == 1 }
+}
+
+#[cfg(windows)]
+pub fn is_stderr_interactive() -> bool {
+    false
+}
---
+++ src/uucore/lib.rs
@@ -0,0 +1,31 @@
+#[cfg(feature = "libc")]
+pub extern crate libc;
+
+#[macro_use]
+mod macros;
+
+#[macro_use]
+pub mod coreopts;
+
+#[cfg(feature = "fs")]
+pub mod fs;
+#[cfg(feature = "utf8")]
+pub mod utf8;
+#[cfg(feature = "encoding")]
+pub mod encoding;
+#[cfg(feature = "parse_time")]
+pub mod parse_time;
+
+#[cfg(all(unix, feature = "utmpx"))]
+pub mod utmpx;
+#[cfg(all(unix, feature = "utsname"))]
+pub mod utsname;
+#[cfg(all(unix, feature = "entries"))]
+pub mod entries;
+#[cfg(all(unix, feature = "process"))]
+pub mod process;
+#[cfg(all(unix, feature = "signals"))]
+pub mod signals;
+
+#[cfg(all(windows, feature = "wide"))]
+pub mod wide;
